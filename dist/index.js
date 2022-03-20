cd470deb19275c7fac8301e6ed5e45a05de2a1e3
0nepeop1e <engshun.lim@gmail.com>
2022-03-20 21:27:43 +0800

    export api for customization

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.utils = exports.TunnelService = void 0;
+var service_1 = require("./service");
+Object.defineProperty(exports, "TunnelService", { enumerable: true, get: function () { return service_1.TunnelService; } });
+exports.utils = require("./utils");
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {Contracts} from "./contracts";
+export {TunnelService} from "./service";
+export * as utils from './utils';
